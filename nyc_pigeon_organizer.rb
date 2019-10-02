8b89c63ad0c7649b0c0a57a2a3acf784657d776a
Jordan Long <jordanleelong5@gmail.com>
2019-10-02 19:57:30 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,29 +2,28 @@ require 'pry'
 def nyc_pigeon_organizer(data)
 pigeon_list = Hash.new
 
-iterate over pigeon data key value pairs
+#iterate over pigeon data key value pairs
 data.each do |key, value|
 
-info: each first level key becomes the key for the nested hash in new pigeon_list
-store this value in a variable (A),
+#info: each first level key becomes the key for the nested hash in new pigeon_list
+#store this value in a variable (A),
 pigeon_data_key = key
 
 
 
-for each of those key|value pairs, iterate on the value to go to the next nested level
 value.each do |key, value|
-  info: each second level key becomes the value for the nested hash in new pigeon list
-  store this value in a variable (B)
+  #info: each second level key becomes the value for the nested hash in new pigeon list
+  #store this value in a variable (B)
   pigeon_data_value = key
 
 
-  info: each second level value becomes the top level key (name) for the pigeon in new pigeon list
-  this is an array of names that needs to be iterated over
+  #info: each second level value becomes the top level key (name) for the pigeon in new pigeon list
+  #this is an array of names that needs to be iterated over
 value.each do |pigeon_name|
 
-  for each name in an array, check to see if the pigeon with that name already exists in new pigeon list
-    if no create new key in pigeon list hash for the name of the pigeon
-      if yes add previously stored key (variable A) | value (variable B) pair directly to the correct pigeon in the pigeon list
+  #for each name in an array, check to see if the pigeon with that name already exists in new pigeon list
+    #if no create new key in pigeon list hash for the name of the pigeon
+      #if yes add previously stored key (variable A) | value (variable B) pair directly to the correct pigeon in the pigeon list
 
         if pigeon_list[pigeon_name].has_key[pigeon_name]
           pigeon_list[pigeon_name] = Hash.new
